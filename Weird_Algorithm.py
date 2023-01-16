0fb374e90d5779bf3b0ff8863bef4ea3551fa6bd
Eklas Mohin <120573102+BuggyLyfe@users.noreply.github.com>
2023-01-16 18:47:21 +0600

    Weird Algorithm

--- Weird_Algorithm.py
+++ Weird_Algorithm.py
@@ -1,11 +1,11 @@
-n = input()
-n = int(n)
+n = int(input())
+ls = []
 while True:
-    print(n)
-    if(n==1):
+    ls.append(n)
+    if n==1:
         break
     elif n%2==1:
-        n  = n * 3 + 1
+        n = n*3+1
     else:
-        n = n / 2
-        n = int(n)
+        n = int(n/2)
+print(*ls)
